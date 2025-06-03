b52e545fee395afac5b5a0df9cb3d40011ab7fc0
Altair-A94 <altairex500@hotmail.com>
2025-06-03 19:56:05 -0300

    App.js att
    Fazendo o app abrir direto na homescreen

--- Projeto/App.js
+++ Projeto/App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { getC, addC, delC } from './componentes/Lista de Carros';
+import { StyleSheet } from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <HomeScreen />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
