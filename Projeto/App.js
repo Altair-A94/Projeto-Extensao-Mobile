d9a73b8cefaa8a513f117e522b0441a79caab52f
Amanda Lima <60403792+AmandaLima18@users.noreply.github.com>
2025-06-05 11:28:25 -0300

    Add files via upload
    App.js atualizado

--- Projeto/App.js
+++ Projeto/App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { getC, addC, delC } from './componentes/Lista de Carros';
+import React, { createContext, useState } from 'react';
+import { Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function App() {
+import HomeScreen from './Screens/HomeScreen';
+import FormScreen from './Screens/FormScreen';
+import TarefasScreen from './Screens/TarefasScreen';
+
+export const TarefasContext = createContext();
+
+const Tab = createMaterialTopTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function Tabs({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: { backgroundColor: '#1c1c1e' },
+        tabBarLabelStyle: { fontWeight: '600', fontSize: 14 },
+        tabBarActiveTintColor: '#ffd60a',
+        tabBarInactiveTintColor: '#fff',
+        tabBarIndicatorStyle: {
+          backgroundColor: '#ffd60a',
+          height: 3,
+          borderRadius: 2,
+        },
+        tabBarShowIcon: true,
+        tabBarIcon: ({ color }) => {
+          let iconName;
+          if (route.name === 'Início') {
+            iconName = 'home';
+          } else if (route.name === 'Minhas Tarefas') {
+            iconName = 'clipboard-list';
+          }
+          return <Icon name={iconName} size={20} color={color} />;
+        },
+        headerRight: () => (
+          route.name === 'Início' ? (
+            <Button onPress={() => navigation.navigate('Form', { modoEdicao: false })}
+              title="Criar"
+              color="#ffd60a"
+            />
+          ) : null
+        ),
+      })}
+    >
+      <Tab.Screen name="Início" component={HomeScreen} />
+      <Tab.Screen name="Minhas Tarefas" component={TarefasScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [tarefas, setTarefas] = useState([]);
+
+  const salvarTarefa = (tarefa) => {
+  if (tarefa.deletar) {
+    setTarefas(old => old.filter(t => t.id !== tarefa.id));
+  } else {
+    setTarefas(old =>
+      old.some(t => t.id === tarefa.id)
+        ? old.map(t => (t.id === tarefa.id ? tarefa : t))
+        : [...old, tarefa]
+    );
+  }
+};
+
+  return (
+    <TarefasContext.Provider value={{ tarefas, salvarTarefa }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Tabs"
+            component={Tabs}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Form"
+            component={FormScreen}
+            options={{
+              title: 'Formulário',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TarefasContext.Provider>
+  );
+}
